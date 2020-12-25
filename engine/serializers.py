f3ba058cefdc5e0f4d0a998b8a18d92a8ac52561
Rakesh Narsayya <rakesh.gunduka@gmail.com>
2020-12-26 01:14:42 +0530

    Changes: - Lesson Model Finalized - LessonSlots Model Finalized - Lesson Create API Implemented - Slot Creation Logic Implemeted for multisessions (Assumption - Multiple Session timings [starttime: endtime] are for each day) - Added S3Storage location for LessonCoverImage

--- engine/serializers.py
+++ engine/serializers.py
@@ -1,6 +1,30 @@
 from rest_framework import serializers
 
+from engine.models import LessonData, LessonSlots
+
 
 # ********* Lessons Serializers **********
-class LessonDataSerializer(serializers.ModelSerializer):
-    pass
+class LessonCreateSerializer(serializers.ModelSerializer):
+    '''
+    Lesson Create Serializer
+    name -> required
+    '''
+    name = serializers.CharField(required=True)
+
+    class Meta:
+        model = LessonData
+        exclude = [
+            'creator'
+        ]
+
+
+class LessonSlotCreateSerializer(serializers.ModelSerializer):
+    '''
+    Lesson Slot Serializer
+    '''
+    class Meta:
+        model = LessonSlots
+        exclude = [
+            'creator',
+            'lesson'
+        ]
