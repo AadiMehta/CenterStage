55699735bded29ca3081fe41d737838377406547
Rakesh Narsayya <rakesh.gunduka@gmail.com>
2021-02-04 00:42:25 +0530

    Changes: - Side bar expand on my account and my schedule - Show lessons only corresponding to the teacher - Logout Functionality - Logout and Delete accounts link - there should be a popup

---
+++ frontend/static/js/account/info.js
@@ -0,0 +1,119 @@
+
+/**
+ * Hide all Modals
+ */
+function hideAll() {
+  $('.modal').map((index, modalEl) => {
+      $(`#${modalEl.id}`).modal('hide');
+  })
+}
+
+/**
+ * Show a specific modal using modal id
+ * @param {String} modalName 
+ * @param {Boolean} hide 
+ */
+function showModal(modalName, hide) {
+  if (hide) {
+      hideAll();
+  }
+  $(`#${modalName}`).modal('toggle');
+  $(`#${modalName}`).modal('show');
+}
+
+/**
+* Hide a specific modal using modal id
+* @param {String} modalName 
+*/
+function hideModal(modalName) {
+  $(`#${modalName}`).modal('toggle');
+  $(`#${modalName}`).modal('hide');
+}
+
+/**
+ * Set Cookie in the browser
+ */
+function setCookie(cname, cvalue, exMins) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exMins*60*1000));
+  var expires = "expires="+d.toUTCString();  
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+/**
+ * Remove auth_token from cookies and route to main page
+ */
+function logout() {
+  setCookie('auth_token', '', 0)
+  document.location.href = '/';
+}
+
+/**
+ * Get Cookie by Name
+ * @param {String} cname 
+ */
+function getCookie(cname) {
+  /**
+   * Gets Cookie from cookie name
+   */
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+/**
+ * Opens Popup window as the current window as parent
+ */
+function openWindow(url, winName, w, h, scroll){
+  LeftPosition = (screen.width) ? (screen.width-w)/2 : 0;
+  TopPosition = (screen.height) ? (screen.height-h)/2 : 0;
+  settings =
+      'height='+h+',width='+w+',top='+TopPosition+',left='+LeftPosition+',scrollbars='+scroll+',resizable'
+  window.open(url, winName, settings)
+}    
+
+
+/**
+ * Connect Zoom Account Handler
+ */
+function handleZoomConnectAccount () {
+  const {redirectUri} = event.target.dataset;
+  const url = `https://zoom.us/oauth/authorize?response_type=code&client_id=mAkYlnKISSCqOgSJPIxCCA&redirect_uri=${redirectUri}`;
+  openWindow(url, 'Authorize Zoom', 600, 700, 1);
+}
+
+/**
+ * Disconnect Zoom Account handler
+ */
+function handleZoomDisconnectAccount (event) {
+  const token = getCookie('auth_token');
+  $.ajax('/api/profile/zoom/disconnect', {
+    type: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    success: function (data, status, xhr) {
+      location.reload();
+    },
+    error: function (jqXhr, textStatus, errorMessage) {
+      alert('err')
+    }
+  });
+}
+
+function init() {
+  $('#logoutButton').click(() => showModal('logoutConfirmation'));
+  $('#logout').click(() => logout());
+  $('#disconnectZoomAccount').click(handleZoomDisconnectAccount);
+  $('#connectZoomAccount').click(handleZoomConnectAccount);
+}
+
+init();
--- frontend/views/dashboard.py
+++ frontend/views/dashboard.py
@@ -1,9 +1,10 @@
+import urllib
 from django.conf import settings
 from django.views.generic import TemplateView
 from frontend.utils import get_user_from_token
 from engine.models import LessonData
 from engine.serializers import LessonSerializer
- 
+from frontend.utils import get_user_from_token, is_authenticated
 
 class DashboardAccountAlerts(TemplateView):
     """
@@ -28,6 +29,20 @@ class DashboardAccountInfo(TemplateView):
         context = super().get_context_data(**kwargs)
         if 'auth_token' in self.request.COOKIES:
             context['user'] = get_user_from_token(self.request.COOKIES.get('auth_token'))
+        if 'auth_token' in self.request.COOKIES:
+            user = get_user_from_token(self.request.COOKIES.get('auth_token'))
+            teacher_accounts = {}
+            accounts = user.teacher_profile_data.accounts.all()
+            for account in accounts:
+                teacher_accounts[account.account_type] = account
+            context.update({
+                'user': user,
+                'teacher_accounts': teacher_accounts,
+                'zoom': {
+                    'ZOOM_CLIENT_ID': settings.ZOOM_CLIENT_ID,
+                    'ZOOM_REDIRECT_URL': urllib.parse.quote_plus(settings.ZOOM_REDIRECT_URL)
+                }
+            })
         context['site_name'] = settings.SITE_URL
         return context
 
@@ -78,13 +93,20 @@ class DashboardLessons(TemplateView):
     template_name = "dashboard/lessons.html"
 
     def get_context_data(self, **kwargs):
+        user = self.get_user()
         context = super().get_context_data(**kwargs)
-        lessons = LessonData.objects.all().order_by('-created_at')
+        lessons = LessonData.objects.filter(creator=user.teacher_profile_data).order_by('-created_at')
         serializer = LessonSerializer(lessons, many=True)
         context['lessons'] = serializer.data
         if 'auth_token' in self.request.COOKIES:
             context['user'] = get_user_from_token(self.request.COOKIES.get('auth_token'))
         return context
+
+    def get_user(self):
+        if is_authenticated(self.request.COOKIES.get('auth_token')):
+            return get_user_from_token(self.request.COOKIES.get('auth_token'))
+        else:
+            return False
 
 
 class DashboardMessages(TemplateView):
